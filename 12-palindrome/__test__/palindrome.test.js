dceb347b1b853129781e9d07403914fdde47ebf9
Taylor Stemple <taylorstemple@gmail.com>
2018-03-01 22:47:42 -0800

    refactor

--- 12-palindrome/__test__/palindrome.test.js
+++ 12-palindrome/__test__/palindrome.test.js
@@ -26,11 +26,34 @@ let list = {
   }
 }
 
+let listTwo = {
+  val: 1,
+  next: {
+    val: 2,
+    next: {
+      val: 3,
+      next: {
+        val: 4,
+        next: null
+      }
+    }
+  }
+}
+
+console.log(isPalindrome(list));
+
 describe('Linked List Palindrome Module', function() {
   describe('#isPalindrome function', function() {
     describe('with a valid, true argument', function() {
       it('should return true if linked list is palindrome', function(done) {
+        
         expect(isPalindrome(list)).toEqual(true);
+        done();
+      });
+    });
+    describe('with a false argument', function() {
+      it('should return false', function(done) {
+        expect(isPalindrome(listTwo)).toEqual(false);
         done();
       });
     });
--- 12-palindrome/solution.js
+++ 12-palindrome/solution.js
@@ -2,13 +2,24 @@
 
 module.exports = function(ll) {
   let array = [];
+  let reversedArr = [];
   let current = ll;
 
   while(current !== null){
     array.push(current.val);
     current = current.next
   }
-  return array === array.reverse();
+
+  for(var i = array.length-1; i >= 0; i-- ) {
+    reversedArr.push(array[i]);
+  }
+  
+  for(var i = 0; i < array.length; i++) {
+    if(array[i] !== reversedArr[i]){
+      return false;
+    }
+  }
+  return true;
 }
 
   
